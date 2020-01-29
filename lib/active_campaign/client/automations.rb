85fa7d3c15a3b66d54bbb22c938fc2077ae73f5a
gitfixes <49126804+gitfixes@users.noreply.github.com>
2020-01-28 18:54:14 -0600

    Update automations.rb

--- lib/active_campaign/client/automations.rb
+++ lib/active_campaign/client/automations.rb
@@ -3,12 +3,17 @@
 module ActiveCampaign
     class Client
         module Automations  
+            
+          def automation_contact_add(options = {})
+            post __method__, options
+          end
+            
             #
             # GET methods
             #
         
             def automation_list(options = {})
-            get __method__, options
+                get __method__, options
             end
   
         end
